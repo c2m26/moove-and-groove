dd4babad207dff2a64a5a9be98035cf556ba19e8
carloscmartins <carlos@c2m-webdev.com>
2019-10-28 16:19:37 +0100

    activities controller populated

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,48 @@
 class ActivitiesController < ApplicationController
+
+def index
+    @activities = Activity.all
+end
+
+def show
+    @activity = Activity.find(params[:id])
+end
+
+def new
+    @activity = Activity.new
+end
+
+def edit
+    @activity = Activity.find(params[:id])
+end
+
+def create
+    @activity = Activity.new(activity_params)
+    if @activity.save
+        redirect_to @activity
+    else
+        render 'new'
+    end
+end
+
+def update
+    @activity = Activity.find(params[:id])
+    if article.update(activity_params)
+        redirect_to @activity
+    else
+        render 'edit'
+end
+
+def destroy
+    @activity = Activity.find(params[:id])
+    @activity.destroy
+
+    redirect_to activities_path
+end
+
+private
+    def activity_params
+        params.require(:activity).permit(:name, :date, :duration)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'site/index'
 
   root 'site#index'
-  
+
+  resources :activities
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
