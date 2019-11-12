352719073f44ad15d044dc860309af4d87a434f2
carloscmartins <carlos@c2m-webdev.com>
2019-11-12 10:33:34 +0100

    view and edit user draft views

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,12 @@ Rails.application.routes.draw do
   root 'site#index'
 
   resources :activities
+  
   devise_for :users
+
+  resources :users
+
+ #get  '/profile', to: 'users#show' 
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
