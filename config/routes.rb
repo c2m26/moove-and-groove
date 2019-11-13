e7db59a88c1f2fcaad54f9d08a9b569d35d21367
carloscmartins <carlos@c2m-webdev.com>
2019-11-13 17:42:52 +0100

    working user profile

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,7 @@ Rails.application.routes.draw do
   
   devise_for :users
 
-  resources :users
-
- #get  '/profile', to: 'users#show' 
+ get  '/profile', to: 'users#show' 
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
